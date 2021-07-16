9048fa939fa9515af4fb18449b72c685dcb2b626
MrZloHex <zlo.alex.it@gmail.com>
2021-07-16 17:22:01 +0300

    SOMETHING

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![allow(non_snake_case)]
 
 mod user_node;
+use std::usize;
+
 use user_node::user::User;
 
 use crate::user_node::user;
@@ -13,8 +15,50 @@ use libp2p::{
     noise::{Keypair, NoiseConfig, X25519Spec},
     tcp::TokioTcpConfig,
     core::upgrade,
-    mplex
+    mplex,
+    floodsub::{Floodsub, FloodsubEvent, Topic},
+    mdns::{TokioMdns},
+    NetworkBehaviour
 };
+
+use tokio::{
+    fs,
+    io::AsyncBufRead,
+    sync::mpsc
+};
+
+
+struct Message {
+    id: usize,
+    theme: String,
+    data: String,
+    public: bool
+}
+
+
+enum ListMode {
+    ALL,
+    One(String),
+}
+
+
+struct ListResponse {
+    mode: ListMode,
+    data: Message,
+    receiver: String,
+}
+
+#[derive(NetworkBehaviour)]
+struct MessageBehaviour {
+    floodsub: Floodsub,
+    mdns: TokioMdns,
+    #[behaviour(ignore)]
+    response_sender: mpsc::UnboundedSender<ListResponse>
+}
+
+
+
+
 
 
 #[tokio::main]
@@ -26,8 +70,11 @@ async fn main() {
         "user".to_string()
     };
 
+    let (response_sender, mut response_rcv) = mpsc::unbounded_channel();
+
     let keys = identity::Keypair::generate_ed25519();
     let peer_id = PeerId::from(keys.public());
+    let topic = Topic::new("message");
 
     let auth_keys = Keypair::<X25519Spec>::new()
         .into_authentic(&keys)
@@ -38,4 +85,14 @@ async fn main() {
         .authenticate(NoiseConfig::xx(auth_keys).into_authenticated())
         .multiplex(mplex::MplexConfig::new())
         .boxed();
+
+    let mut behavior = MessageBehaviour {
+        floodsub: Floodsub::new(peer_id.clone()),
+        mdns: TokioMdns::new().expect("Can create mdns"),
+        response_sender,
+    };
+
+    behavior.floodsub.subscribe(topic.clone());
+
+
 }
